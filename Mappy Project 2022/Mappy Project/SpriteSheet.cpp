dca61d346b5413b3e9a4522d254b25e9c78a5892
percymiltier <percymiltier@gmail.com>
2025-06-14 15:41:35 -0400

    fix jumping sprites

--- Mappy Project 2022/Mappy Project/SpriteSheet.cpp
+++ Mappy Project 2022/Mappy Project/SpriteSheet.cpp
@@ -32,64 +32,6 @@ void Sprite::InitSprites(int width, int height)
 	al_convert_mask_to_alpha(image, al_map_rgb(255,0,255));
 }
 
-void Sprite::UpdateSprites(int width, int height, int dir)
-{
-	int oldx = x;
-	int oldy = y;
-
-	if(dir == 1){ //right key
-		animationDirection = 1; 
-		x+=2; 
-		if (++frameCount > frameDelay)
-		{
-			frameCount=0;
-			if (++curFrame > walkmaxframe)
-				curFrame=1;
-		}
-	} else if (dir == 0){ //left key
-		animationDirection = 0; 
-		x-=2; 
-		if (++frameCount > frameDelay)
-		{
-			frameCount=0;
-			if (++curFrame > walkmaxframe)
-				curFrame=1;
-		}
-	}else //represent that they hit the space bar and that mean direction = 0
-		animationDirection = dir;
-
-	// jump
-	if (lastjump > y) {
-		// ascending
-		curFrame = 10;
-	}
-	else if (lastjump < y) {
-		// descending
-		curFrame = 11;
-	}
-	if (ifjump && collidedTL(x, y)) {
-		curFrame = 12;
-		ifjump = false;
-	}
-
-	//check for collidedTL with foreground tiles
-	if (animationDirection == 0)
-	{ 
-		if (collidedTL(x, y + frameHeight)) { //collision detection to the left
-			x = oldx; 
-			y= oldy;
-		}
-
-	}
-	else if (animationDirection == 1)
-	{ 
-		if (collidedTL(x + frameWidth, y + frameHeight)) { //collision detection to the right
-			x = oldx; 
-			y= oldy;
-		}
-	}
-}
-
 bool Sprite::CollisionEndBlock()
 {
 	if (endValue(x + frameWidth/2, y + frameHeight + 5))
@@ -117,39 +59,74 @@ void Sprite::DrawSprites(int xoffset, int yoffset)
 	}
 }
 
-int Sprite::jumping(int jump, const int JUMPIT)
-{
-	//handle jumping
-	if (jump==JUMPIT) { 
-		if (!collidedTL(x + frameWidth/2, y + frameHeight + 5))
-			jump = 0; 
-	}
-	else
-	{
-		y -= jump/3; 
-		jump--; 
-		curFrame=0;
-	}
-
-	if (jump<0) 
-	{ 
-		if (collidedTL(x + frameWidth/2,  y + frameHeight))
-		{ 
-			jump = JUMPIT; 
-			while (collidedTL(x + frameWidth/2,y + frameHeight))
-			{
-				y -= 3;
-			}
-		}
-
-	}
-
-	// keep jump from escaping map
-	if (y < 0) {
-		y = 0;
-		jump = 0;
-	}
-
-	lastjump = y;
-	return jump;
+void Sprite::UpdateSprites(int width, int height, int dir) {
+	int oldx = x; int oldy = y; 
+	if (dir == 1) { 
+		//right key 
+		animationDirection = 1; 
+		x += 2; 
+		if (++frameCount > frameDelay) { 
+			frameCount = 0; 
+			if (++curFrame > maxFrame)
+				curFrame = 1; 
+		} 
+	} else if (dir == 0) { 
+		//left key 
+		animationDirection = 0; 
+		x -= 2; 
+		if (++frameCount > frameDelay) { 
+			frameCount = 0; 
+			if (++curFrame > maxFrame) 
+				curFrame = 1; 
+		} 
+	} else 
+		//represent that they hit the space bar and that mean direction = 0 
+		animationDirection = dir; 
+		//check for collided with foreground tiles 
+		if (animationDirection == 0) { 
+			if (collidedTL(x, y + frameHeight)) {
+				//collision detection to the left 
+				x = oldx - 2;
+				y = oldy; 
+			} 
+		} else if (animationDirection == 1) { 
+			if (collidedTL(x + frameWidth, y + frameHeight)) {
+				//collision detection to the right 
+				x = oldx - 2; 
+				y = oldy; 
+			} 
+		} 
 }
+
+
+int Sprite::jumping(int jump, const int JUMPIT) { 
+	//handle jumping 
+	if (jump == JUMPIT) { 
+		if (!collidedTL(x + frameWidth / 2, y + frameHeight + 5))
+			jump = 0; 
+	} else { 
+		y -= jump / 3;
+		if (y < 0) 
+			y = 0; 
+			jump--;
+		if (jump > 18) 
+			curFrame = 8; 
+		else if (jump > 10) 
+			curFrame = 9; 
+		else if (jump > 3) 
+			curFrame = 10; 
+		else curFrame = 11; 
+	} if (jump < 0) { 
+		if (collidedTL(x + frameWidth / 2, y + frameHeight)) {
+			jump = JUMPIT; 
+			while (collidedTL(x + frameWidth / 2, y + frameHeight)) {
+				y -= 3; 
+				if (y < 0) 
+					y = 0; 
+			}
+		} 
+	} 
+	return jump; 
+}
+
+
